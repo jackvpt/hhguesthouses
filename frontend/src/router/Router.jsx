4345221b2a2467ddeb0c5a89e2b60a099c64fc2b
jackvpt <jacques.verpoest@gmail.com>
2025-08-09 22:54:14 +0200

    isAuthenticated

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -4,8 +4,9 @@ import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 import Signup from "../Pages/Signup/Signup"
 import Login from "../Pages/Login/Login"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import Error404 from "../Pages/Error404/Error404"
+import { clearUser } from "../features/userSlice"
 
 /**
  * Application router component using React Router v6.
@@ -15,6 +16,13 @@ import Error404 from "../Pages/Error404/Error404"
  * @returns {JSX.Element} The main Router component for the application.
  */
 export default function Router() {
+  const dispatch = useDispatch()
+
+  const token = localStorage.getItem("token") || sessionStorage.getItem("token")
+  if (!token) {
+    dispatch(clearUser())
+  }
+
   const isAuthenticated = useSelector((state) => state.user.userId !== null)
 
   return (
@@ -22,14 +30,20 @@ export default function Router() {
       <Header />
       <main>
         <Routes>
-          <Route
-            path="/"
-            element={isAuthenticated ? <Navigate to="/display" /> : <Login />}
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="display" element={<Display />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="*" element={<Error404 />} />
+          {!isAuthenticated ? (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Display />} />
+              <Route path="/display" element={<Display />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Error404 />} />
+            </>
+          )}
         </Routes>
       </main>
       <Footer />
